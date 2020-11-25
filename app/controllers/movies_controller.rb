bd9ed507ac9b86435cb150aa19c6adcf793ac52b
Sy Rashid <sy.rashid.11@gmail.com>
2020-11-26 00:08:25 +0100

    FEAT: Implemented search functionality with active record

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,18 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
+    if params[:query].present?
+      sql_query = "movies.title @@ :query \
+                   OR movies.syllabus @@ :query \
+                   OR directors.first_name @@ :query \
+                   OR directors.last_name @@ :query"
+      @movies = Movie.joins(:director).where(sql_query, query: "%#{params[:query]}%")
+
+      # sql_query = "title ILIKE :query OR syllabus ILIKE :query"
+      # @movies = Movie.where(sql_query, query: "%#{params[:query]}%")
+
+      # @movies = Movie.where("title ILIKE ? OR syllabus ILIKE ?", "%#{params[:query]}%", "%#{params[:query]}%")
+    else
+      @movies = Movie.all
+    end
   end
 end
