96e103a333dbcd86fd24c4173ec7052167055d3a
Sy Rashid <sy.rashid.11@gmail.com>
2020-11-26 00:20:47 +0100

    FEAT: Implemented basic pg search

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,11 +1,13 @@
 class MoviesController < ApplicationController
   def index
     if params[:query].present?
-      sql_query = "movies.title @@ :query \
-                   OR movies.syllabus @@ :query \
-                   OR directors.first_name @@ :query \
-                   OR directors.last_name @@ :query"
-      @movies = Movie.joins(:director).where(sql_query, query: "%#{params[:query]}%")
+      @movies = Movie.search_by_title_and_syllabus(params[:query])
+
+      # sql_query = "movies.title @@ :query \
+      #              OR movies.syllabus @@ :query \
+      #              OR directors.first_name @@ :query \
+      #              OR directors.last_name @@ :query"
+      # @movies = Movie.joins(:director).where(sql_query, query: "%#{params[:query]}%")
 
       # sql_query = "title ILIKE :query OR syllabus ILIKE :query"
       # @movies = Movie.where(sql_query, query: "%#{params[:query]}%")
